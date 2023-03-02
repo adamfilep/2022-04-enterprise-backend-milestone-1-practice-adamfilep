e4fe07c80a79149076cfdbbfca668ed48e39b73f
adamfilep <adamfilep@gmail.com>
2023-03-02 09:55:34 +0100

    Add Service classes

--- src/main/java/com/codecool/library/service/BookService.java
+++ src/main/java/com/codecool/library/service/BookService.java
@@ -1,2 +1,38 @@
-package com.codecool.library.service;public class BookService {
+package com.codecool.library.service;
+
+import com.codecool.library.repository.BookDao;
+import com.codecool.library.repository.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private BookDao bookDao;
+    @Autowired
+    public BookService(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    public void addBook(Book book) {
+        bookDao.addBook(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookDao.getAllBooks();
+    }
+
+    public int numberOfBooksInGenre(String genre) {
+        return bookDao.numberOfBooksInGenre(genre);
+    }
+
+    public List<Book> listBooksByWritersBirthPlace(String birthPlace) {
+        return bookDao.listBooksByWritersBirthPlace(birthPlace);
+    }
+
+
+    public List<String> listAllBorrowedBooks() {
+        return bookDao.listAllBorrowedBooks();
+    }
 }
--- src/main/java/com/codecool/library/service/WriterService.java
+++ src/main/java/com/codecool/library/service/WriterService.java
@@ -1,2 +1,20 @@
-package com.codecool.library.service;public class WriterService {
+package com.codecool.library.service;
+
+import com.codecool.library.repository.WriterDao;
+import com.codecool.library.repository.model.Writer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WriterService {
+    private WriterDao writerDao;
+
+    @Autowired
+    public WriterService(WriterDao writerDao) {
+        this.writerDao = writerDao;
+    }
+
+    public void addWriter(Writer writer) {
+        writerDao.addWriter(writer);
+    }
 }
