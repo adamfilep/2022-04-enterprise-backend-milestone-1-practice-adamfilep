a876374c7c79d1f3db0e25d1ae7f88862c0d2ea0
adamfilep <adamfilep@gmail.com>
2023-03-02 10:20:42 +0100

    Add implement BookController

--- src/main/java/com/codecool/library/controller/BookController.java
+++ src/main/java/com/codecool/library/controller/BookController.java
@@ -2,18 +2,34 @@ package com.codecool.library.controller;
 
 import com.codecool.library.repository.model.Book;
 import com.codecool.library.service.BookService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
 public class BookController {
     private BookService bookService;
 
+    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
-    @PostMapping("/book")
+    @PostMapping()
     public void addBook(@RequestBody Book book) {
         bookService.addBook(book);
+    }
+
+    @GetMapping()
+    public int numberOfBooksByGenre(@RequestParam("genre") String genre) {
+        return bookService.numberOfBooksInGenre(genre);
+    }
+
+    @GetMapping("/allBorrowed")
+    public List<String> listAllBorrowedBooks() {
+        return bookService.listAllBorrowedBooks();
     }
 }
